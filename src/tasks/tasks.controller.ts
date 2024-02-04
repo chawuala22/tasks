e80ca05e4b874e6da2e2cddb87c19146646ed65f
MiguelR <miguel.rojas@castor.com.co>
2024-02-04 09:48:28 -0500

    API REST Complete

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto, UpdateTaskDTto } from './dto/task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private _tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this._tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this._tasksService.createTasks(newTask.title, newTask.description);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string) {
+    return this._tasksService.deleteTasks(id);
+  }
+
+  @Patch(':id')
+  updateTask(@Param('id') id: string, @Body() updateFields: UpdateTaskDTto) {
+    return this._tasksService.updateTasks(id, updateFields);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid';
+import { UpdateTaskDTto } from './dto/task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'First Task',
+      description: 'Description task for the first task',
+      status: TaskStatus.PENDING,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  getTaskById(taskId: string): Task {
+    return this.tasks.find((task) => task.id === taskId);
+  }
+  createTasks(title: string, description: string) {
+    const task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+  deleteTasks(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTasks(id: string, updateFields: UpdateTaskDTto): Task {
+    const task = this.getTaskById(id);
+    const newtask = Object.assign(task, updateFields);
+    this.tasks = this.tasks.map((task) => (task.id === id ? newtask : task));
+    return newtask;
+  }
+}
